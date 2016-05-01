371b16184167c64a81dfffcba42754f2afb92b0c
Nick Nelson <nicknlsn@gmail.com>
2016-04-30 22:28:02 -0600

    done for now

--- pages/homepage.php
+++ pages/homepage.php
@@ -14,6 +14,7 @@ include 'modules/navBar.html';
 		<div class="container">
 			<h2>Nick Nelson</h2>
 			<a href="https://play.spotify.com/track/4KGeEIOooF3lH48Vk5tRxh">blah blah blah</a>
+	      	<p><p><img src="../images/funny-picture.jpg" alt="funny picture"></p></p>
 		</div>
 	</body>
 </html>
