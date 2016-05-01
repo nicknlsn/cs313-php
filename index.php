92f386d73e39e48fc76c4555c2053caf4f665f14
Nick Nelson <nicknlsn@gmail.com>
2016-04-30 20:09:24 -0600

    started homepage

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
+<?php
+
+
+?>
 <DOCTYPE html>
 <html lang="en">
 	<head>
+		<title>Homepage</title>
 		<meta charset="utf-8">
-		<title>yo diggy wiggy</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/myStyles.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 	</head>
-
 	<body>
-		<h1>A Simple Hello World</h1>
-		<p>Nick was here.</p>
+
+<?php
+include 'modules/navBar.html'
+
+?>
+
+		<div class="container">
+			<h3>Nick Nelson</h3>
+			<p>blah blah blah</p>
+		</div>
 	</body>
 </html>
