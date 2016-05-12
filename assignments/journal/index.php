1ef82165a78c0f6a92e3fc3ac30b3407f848dc0a
Nick Nelson <nicknlsn@gmail.com>
2016-05-12 10:40:46 -0600

    added test directory

---
+++ assignments/journal/index.php
@@ -0,0 +1,30 @@
+<?php
+// session handling here
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include 'modules/headBlock.html';
+?>
+
+<body>
+
+<!-- top nav bar -->
+<?php
+include 'modules/navBarTop.html';
+?>
+
+<div class="container">
+  <h3>New Entry</h3>
+</div>
+
+<!-- Static bottom navbar -->
+<?php
+// include 'modules/navBarBottom.html'; // not sure if i need a bottom bar
+?>
+
+</body>
+</html>
---
+++ test/showDatabaseInso.php
@@ -0,0 +1,3 @@
+<?php
+echo "Connection URL: mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/";
+?>
